55150a6a2bb4f9be7590be4e01801ac79a658c81
Marco Aurélio <marco@asarch.localdommain>
2019-10-19 19:06:49 -0300

    Saving serial data with array

--- receptionApp/receptionApp.ino
+++ receptionApp/receptionApp.ino
@@ -1,16 +1,22 @@
-const int btn = 4;  
-byte byteRead;
-int buttonState = 0; 
+/* 
+ * Reading Serial data with array
+ */
 
+const int btn = 8;  
+int  serIn;      
+int  serInIndx  = 0;  
+int  serOutIndx = 0;
+int buttonState = 0; 
 boolean lastButton = LOW;
 boolean currentButton = LOW;  
-                                                             
-byte message[] = { 0x05,0x64,0x0b,0xc4,0x03,0x00,0x01,0x00,0x6b,0xe6,0xc0,0xc0,0x01,0x3c,0x01,0x06,0xff,0x50 };
-char pkg[500];
 
-void setup() {                 
-    Serial.begin(9600);
-    pinMode(btn, INPUT);
+byte serInString[400];
+byte byteRead;
+byte message[] = { 0x05,0x64,0x0b,0xc4,0x03,0x00,0x01,0x00,0x6b,0xe6,0xc0,0xc0,0x01,0x3c,0x01,0x06,0xff,0x50 };
+
+void setup() {
+  pinMode(btn, INPUT);
+  Serial.begin(9600); 
 }
 
 boolean debounce( boolean last ){
@@ -21,20 +27,32 @@ boolean debounce( boolean last ){
     }
     return current;
 }
- 
-void loop() {
-    currentButton = debounce(lastButton);
-    if( lastButton == LOW && currentButton == HIGH ) { 
-        Serial.println("");
-        Serial.println("Enviando...");
-        Serial.write(message, sizeof(message));     
-    }
-    lastButton = currentButton;
 
-    if ( Serial.available() > 0 ){        
-        byteRead = Serial.read();
-        sprintf(pkg, "%02x", byteRead);
-        Serial.print(pkg);
+void readSerialString () {
+    byte sb;   
+    if( Serial.available() ) { 
+       while ( Serial.available() ){ 
+          sb = Serial.read();             
+          serInString[serInIndx] = sb;
+          serInIndx++;  
+       }
     }
-    
+    if( serInIndx > 0 ) {      
+      for( serOutIndx=0; serOutIndx < serInIndx; serOutIndx++ ) {
+          Serial.print( serInString[serOutIndx] );      
+      }        
+      serOutIndx = 0;
+      serInIndx  = 0;  
+    }
+}
+
+void loop () {
+    currentButton = debounce(lastButton);
+    if(lastButton == LOW && currentButton == HIGH) { 
+        Serial.println("Enviando...");
+        Serial.write(message, sizeof(message)); 
+        //readPkg(); 
+    }
+    lastButton = currentButton;  
+    readSerialString(); 
 }
