8de784055b696b6a055ec5f68e5f8718d8c1fba1
device <sgtmarcoaurelio@icloud.com>
2019-11-19 12:04:38 -0300

    All parameters completed

--- receptionApp/sendApp/sendApp.ino
+++ receptionApp/sendApp/sendApp.ino
@@ -1,8 +1,14 @@
+#include <SoftwareSerial.h>
+
+// Alternative serial port
+SoftwareSerial mySerial(2, 3); 
+
 int nParameter;
 int qResp;
 
 void setup() {
     Serial.begin(9600);  
+    mySerial.begin(9600);
 }
 
 void loop(){
@@ -20,9 +26,13 @@ void getParameter() {
     Serial.println( "* [3] Teporizacao" );
     Serial.println( "* [4] Limitador Max" );
     Serial.println( "* [5] Limitador Min" );
+    Serial.println( "* [6] Bloq. Tap Max" );
+    Serial.println( "* [7] Bloq. Tap Min" );
+    Serial.println( "* [8] RLV" );
+    Serial.println( "* [9] RLI" );
     Serial.println( "-------------------------------------------" );
    
-    while( ( nParameter != '1' ) && ( nParameter != '2' ) && ( nParameter != '3' ) && ( nParameter != '4' ) && ( nParameter != '5' ) ){
+    while( ( nParameter != '1' ) && ( nParameter != '2' ) && ( nParameter != '3' ) && ( nParameter != '4' ) && ( nParameter != '5' ) && ( nParameter != '6' ) && ( nParameter != '7' ) && ( nParameter != '8' ) && ( nParameter != '9' ) ){
         nParameter = Serial.read();
     }
     return nParameter;
@@ -45,6 +55,18 @@ void parameterSetValue( byte selectedParameter ) {
         case '5':
             minLimit();
             break;
+        case '6':
+            maxBTap();
+            break;
+        case '7':
+            minBTap();
+            break;            
+        case '8':
+            changeRlv();
+            break;                
+        case '9':
+            changeRli();
+            break;                  
     }
 }
 
@@ -59,7 +81,7 @@ void changeREFVoltage() {
     while( ( vRef < 100 ) || ( vRef > 130 ) ){
         vRef = Serial.parseInt();
     }
-    sendPkg(vRef, 1); 
+    sendPkg(vRef, 0, 1); 
 }
 
 void Insensitivity(){
@@ -74,10 +96,10 @@ void Insensitivity(){
         insens = Serial.parseFloat();
     }
     if ( insens == 1 || insens == 2 || insens == 3 || insens == 4 || insens == 5 || insens == 6 ) {
-      sendPkg( (int)insens, 2 );   
+      sendPkg( (int)insens, 0,  2 );   
     } else {
       insens *= 10;
-      sendPkg( (int)insens, 2 );   
+      sendPkg( (int)insens, 0, 2 );   
     }
 }
 
@@ -92,7 +114,7 @@ void timing(){
     while( ( vTemp < 10 ) || ( vTemp > 120 ) ){
         vTemp = Serial.parseInt();
     }
-    sendPkg(vTemp, 3); 
+    sendPkg(vTemp, 0, 3); 
 }
 
 void maxLimit(){
@@ -110,9 +132,9 @@ void maxLimit(){
     // turn off the limiter
     if (vMaxLimit == 10){
         vMaxLimit = 0;
-        sendPkg(vMaxLimit, 11); 
+        sendPkg(vMaxLimit, 0, 11); 
     }
-    sendPkg(vMaxLimit, 11); 
+    sendPkg(vMaxLimit, 0, 11); 
 }
 
 void minLimit(){
@@ -129,63 +151,215 @@ void minLimit(){
     // turn off the limiter
     if (vMinLimit == 10){
         vMinLimit = 0;
-        sendPkg(vMinLimit, 12); 
+        sendPkg(vMinLimit, 0, 12); 
     }    
-    sendPkg(vMinLimit, 12);    
+    sendPkg(vMinLimit, 0, 12);    
 }
 
-void sendPkg(int value, int address){
-    byte pkgWrite[ ] = {5, 100, 16, 196, 3, 0, 1, 0, 38, 151, 192 ,192, 5, 41, 2, 23,  1, address, value, 0, 0 ,47, 104 };
+void maxBTap(){
+    int value;
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "Parametro selecionado:  Bloqueio de Tap Max." );
+    Serial.println( "Escolha o valor desejado: " );
+    Serial.println( "* [1] 5%" );           // 08
+    Serial.println( "* [2] 6 1/4%" );    // 0a
+    Serial.println( "* [3]  7 1/2%" );   // 0c
+    Serial.println( "* [4] 8 3/4%" );   // 0e
+    Serial.println( "* [5] 10%" );        // 10
+    Serial.println( "-------------------------------------------" );
+   
+    while( ( value != '1' ) && ( value != '2' ) && ( value != '3' ) && ( value != '4' ) && ( value != '5' ) ){
+        value = Serial.read();
+    }
+
+    switch (value) {
+    case '1':
+        sendPkg(8, 0, 13);    
+        break;
+    case '2':
+        sendPkg(10, 0, 13);
+        break;
+    case '3':
+        sendPkg(12, 0, 13);    
+        break; 
+    case '4':
+        sendPkg(14, 0, 13);    
+        break;       
+    case '5':
+        sendPkg(16, 0, 13);    
+        break;     
+    }
+}
+
+void minBTap(){
+    int value;
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "Parametro selecionado:  Bloqueio de Tap Min." );
+    Serial.println( "Escolha o valor desejado: " );
+    Serial.println( "* [1] 5%" );           // 08
+    Serial.println( "* [2] 6 1/4%" );    // 0a
+    Serial.println( "* [3]  7 1/2%" );   // 0c
+    Serial.println( "* [4] 8 3/4%" );   // 0e
+    Serial.println( "* [5] 10%" );        // 10
+    Serial.println( "-------------------------------------------" );
+   
+    while( ( value != '1' ) && ( value != '2' ) && ( value != '3' ) && ( value != '4' ) && ( value != '5' ) ){
+        value = Serial.read();
+    }
+
+    switch (value) {
+    case '1':
+        sendPkg(8, 0, 14);    
+        break;
+    case '2':
+        sendPkg(10, 0, 14);
+        break;
+    case '3':
+        sendPkg(12, 0, 14);    
+        break; 
+    case '4':
+        sendPkg(14, 0, 14);    
+        break;       
+    case '5':
+        sendPkg(16, 0, 14);    
+        break;
+    }
+}
+
+void changeRlv(){
+    float rlvValue = 0;
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "Parametro selecionado: RLV" );
+    Serial.println( "São aceitos valores de 25.0 à 209.0 " );
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "-> Valor desejado: " );
+    
+    while( ( rlvValue < 25 ) || ( rlvValue > 209 ) ){
+        rlvValue = Serial.parseFloat();
+    }
+    /*
+    if ( ( (int)(rlvValue * 10) % 10 ) == 0) {
+      sendPkg( (int)rlvValue, 15 );   
+    } else {
+      rlvValue *= 10;
+      sendPkg( (int)rlvValue, 15 );   
+    }
+    */
+    rlvValue *= 10;
+    byte tempBuff[2];
+    tempBuff[0] = (int)rlvValue & 255;
+    tempBuff[1] = ((int)rlvValue >> 8)  & 255;
+    sendPkg( tempBuff[0], tempBuff[1], 15 );   
+}
+
+void changeRli(){
+    int rliValue = 0;
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "Parametro selecionado: RLI" );
+    Serial.println( "São aceitos valores de 250 à 6000 " );
+    Serial.println( "-------------------------------------------" );
+    Serial.println( "-> Valor desejado: " );
+    
+    while( ( rliValue < 250 ) || ( rliValue > 6000 ) ){
+        rliValue = Serial.parseInt();
+    }
+    byte tempBuff[2];
+    tempBuff[0] = rliValue & 255;
+    tempBuff[1] = ( rliValue >> 8 ) & 255;
+    sendPkg( tempBuff[0], tempBuff[1], 16 );   
+}
+
+void sendPkg(int valueMSB, int valueLSB, int address){
+    byte pkgWrite[ ] = {5, 100, 16, 196, 3, 0, 1, 0, 38, 151, 192 ,192, 5, 41, 2, 23,  1, address, valueMSB, valueLSB, 0 ,47, 104 };
     switch (address){
     case 1:
         Serial.println("");
         Serial.println( "-------------------------------------------" );
         Serial.print("Modificando a Tensão de referencia para: ");
-        Serial.println(value);
+        Serial.println(valueMSB);
         Serial.println("");
         Serial.println("");
-        pkgWrite[18] = value;
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
         Serial.write( pkgWrite, sizeof( pkgWrite ) );
+        //  Change parameter by alternative serial port
+        // Serial.write( pkgWrite, sizeof( pkgWrite ) );
         break;
     case 2:
         Serial.println("");
         Serial.println( "-------------------------------------------" );
         Serial.print("Modificando a Insensibilidade para: ");
-        Serial.println((float)value/10); // show in decimal 
+        Serial.println((float)valueMSB/10); // show in decimal 
         Serial.println("");
         Serial.println("");
-        pkgWrite[18] = value;
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
         Serial.write( pkgWrite, sizeof( pkgWrite ) );
         break;
     case 3:
         Serial.println("");
         Serial.println( "-------------------------------------------" );
         Serial.print("Ajustando a Temporização para: ");
-        Serial.println(value); // show in decimal 
+        Serial.println(valueMSB); // show in decimal 
         Serial.println("");
         Serial.println("");
-        pkgWrite[18] = value;
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
         Serial.write( pkgWrite, sizeof( pkgWrite ) );
         break;
     case 11:
         Serial.println("");
         Serial.println( "-------------------------------------------" );
         Serial.print("Ajustando VMA para: ");
-        Serial.println(value); // show in decimal 
+        Serial.println(valueMSB); // show in decimal 
         Serial.println("");
         Serial.println("");
-        pkgWrite[18] = value;
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
         Serial.write( pkgWrite, sizeof( pkgWrite ) );
         break;      
     case 12:
         Serial.println("");
         Serial.println( "-------------------------------------------" );
         Serial.print("Ajustando VMI para: ");
-        Serial.println(value); // show in decimal 
+        Serial.println(valueMSB); // show in decimal 
         Serial.println("");
         Serial.println("");
-        pkgWrite[18] = value;
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
         Serial.write( pkgWrite, sizeof( pkgWrite ) );
         break;          
+    case 13:
+        Serial.println("");
+        Serial.println( "-------------------------------------------" );
+        Serial.println("Ajustando Bloq. Tap Max: ");
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
+        Serial.write( pkgWrite, sizeof( pkgWrite ) );
+        break;   
+    case 14:
+        Serial.println("");
+        Serial.println( "-------------------------------------------" );
+        Serial.println("Ajustando Bloq. Tap Min: ");
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
+        Serial.write( pkgWrite, sizeof( pkgWrite ) );
+        break;   
+    case 15:
+        Serial.println("");
+        Serial.println( "-------------------------------------------" );
+        Serial.println("Ajustando RLV: ");
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
+        Serial.write( pkgWrite, sizeof( pkgWrite ) );
+        break;       
+    case 16:
+        Serial.println("");
+        Serial.println( "-------------------------------------------" );
+        Serial.println("Ajustando RLI: ");
+        pkgWrite[18] = valueMSB;
+        pkgWrite[19] = valueLSB;
+        Serial.write( pkgWrite, sizeof( pkgWrite ) );
+        break;                    
     }
 }
